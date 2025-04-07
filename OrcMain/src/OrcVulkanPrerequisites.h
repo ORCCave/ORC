e56a3a5b25845f747a585c48dbf2d48393021130
Uzi Wang <429373960@qq.com>
2025-04-07 14:06:09 +0800

    Add display macro

--- OrcMain/src/OrcVulkanPrerequisites.h
+++ OrcMain/src/OrcVulkanPrerequisites.h
@@ -5,17 +5,24 @@
 #include "OrcTypes.h"
 
 #ifdef ORC_USE_VULKAN
-#if defined(ORC_PLATFORM_WIN32)
-#define VK_USE_PLATFORM_WIN32_KHR
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#elif defined(ORC_PLATFORM_LINUX)
-#define VK_USE_PLATFORM_XLIB_KHR
-#endif
+    #if defined(ORC_PLATFORM_WIN32)
+        #define VK_USE_PLATFORM_WIN32_KHR
+        #ifndef WIN32_LEAN_AND_MEAN
+            #define WIN32_LEAN_AND_MEAN
+        #endif
+        #ifndef NOMINMAX
+            #define NOMINMAX
+        #endif
+    #elif defined(ORC_PLATFORM_LINUX)
+        #if defined(SDL_VIDEO_DRIVER_WAYLAND)
+            #define ORC_USE_WAYLAND
+        #elif defined(SDL_VIDEO_DRIVER_X11)
+            #define ORC_USE_X11
+        #endif
+        #if !defined(ORC_USE_WAYLAND) && !defined(ORC_USE_X11)
+            #error "SDL3 must support at least one of Wayland or X11 on Linux."
+        #endif
+    #endif
 
 #include <vk_mem_alloc.h>
 #include <vulkan/vulkan.hpp>
