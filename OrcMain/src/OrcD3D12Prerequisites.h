cd5190fac0fc1b9e60ef0b110d65f216223ad4b2
Uzi Wang <429373960@qq.com>
2025-04-08 00:53:12 +0800

    Remove display macro and format the code

--- OrcMain/src/OrcD3D12Prerequisites.h
+++ OrcMain/src/OrcD3D12Prerequisites.h
@@ -5,44 +5,47 @@
 #include "OrcTypes.h"
 
 #ifdef ORC_PLATFORM_WIN32
-    #ifndef WIN32_LEAN_AND_MEAN
-        #define WIN32_LEAN_AND_MEAN
-    #endif
-    #ifndef NOMINMAX
-        #define NOMINMAX
-    #endif
-    #include <Windows.h>
 
-    #include <d3d12.h>
-    #include <dxgi1_6.h>
-    #include <wrl/client.h>
-    #include <wrl/wrappers/corewrappers.h>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 
-    #include <combaseapi.h>
-    #include <comdef.h>
-    #include <libloaderapi.h>
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 
-    #include "OrcException.h"
+#include <Windows.h>
 
-    namespace Orc
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl/client.h>
+#include <wrl/wrappers/corewrappers.h>
+
+#include <combaseapi.h>
+#include <comdef.h>
+#include <libloaderapi.h>
+
+#include "OrcException.h"
+
+namespace Orc
+{
+    inline void CHECK_DX_RESULT(HRESULT hr)
     {
-        inline void CHECK_DX_RESULT(HRESULT hr)
+        if (FAILED(hr))
         {
-            if (FAILED(hr))
-            {
-    #ifdef _DEBUG
-                std::string s = std::format("(0x{:08x}) ", static_cast<unsigned int>(hr));
-                _com_error err(hr);
-                LPCTSTR errMsg = err.ErrorMessage();
-                throw OrcException(s + errMsg);
-    #endif
-            }
+#ifdef _DEBUG
+            std::string s = std::format("(0x{:08x}) ", static_cast<unsigned int>(hr));
+            _com_error err(hr);
+            LPCTSTR errMsg = err.ErrorMessage();
+            throw OrcException(s + errMsg);
+#endif
         }
-
-        std::shared_ptr<GraphicsDevice> createD3D12GraphicsDevice(void* hwnd, uint32 width, uint32 height);
-        std::shared_ptr<GraphicsCommandList> createD3D12CommandList(GraphicsDevice* device, GraphicsCommandList::GraphicsCommandListType type);
     }
 
+    std::shared_ptr<GraphicsDevice> createD3D12GraphicsDevice(void* hwnd, uint32 width, uint32 height);
+    std::shared_ptr<GraphicsCommandList> createD3D12CommandList(GraphicsDevice* device, GraphicsCommandList::GraphicsCommandListType type);
+}
+
 #else
-    namespace Orc { inline std::shared_ptr<GraphicsDevice> createD3D12GraphicsDevice(void* hwnd, uint32 width, uint32 height) { return std::shared_ptr<GraphicsDevice>(); } }
+namespace Orc { inline std::shared_ptr<GraphicsDevice> createD3D12GraphicsDevice(void* hwnd, uint32 width, uint32 height) { return std::shared_ptr<GraphicsDevice>(); } }
 #endif
--- OrcMain/src/OrcVulkanPrerequisites.h
+++ OrcMain/src/OrcVulkanPrerequisites.h
@@ -5,34 +5,29 @@
 #include "OrcTypes.h"
 
 #ifdef ORC_USE_VULKAN
-    #if defined(ORC_PLATFORM_WIN32)
-        #define VK_USE_PLATFORM_WIN32_KHR
-        #ifndef WIN32_LEAN_AND_MEAN
-            #define WIN32_LEAN_AND_MEAN
-        #endif
-        #ifndef NOMINMAX
-            #define NOMINMAX
-        #endif
-    #elif defined(ORC_PLATFORM_LINUX)
-        #include <build_config/SDL_build_config.h>
-        #if defined(SDL_VIDEO_DRIVER_WAYLAND)
-            #define ORC_USE_WAYLAND
-        #elif defined(SDL_VIDEO_DRIVER_X11)
-            #define ORC_USE_X11
-        #endif
-        #if !defined(ORC_USE_WAYLAND) && !defined(ORC_USE_X11)
-            #error "SDL3 must support at least one of Wayland or X11 on Linux."
-        #endif
-    #endif
 
-    #include <vk_mem_alloc.h>
-    #include <vulkan/vulkan.hpp>
+#ifdef ORC_PLATFORM_WIN32
 
-    namespace Orc
-    {
-        std::shared_ptr<GraphicsDevice> createVulkanGraphicsDevice(void* windowHandle, uint32 width, uint32 height);
-        std::shared_ptr<GraphicsCommandList> createVulkanCommandList(GraphicsDevice* device, VkCommandPool cmdPool, GraphicsCommandList::GraphicsCommandListType type);
-    }
+#define VK_USE_PLATFORM_WIN32_KHR
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#endif
+
+#include <vk_mem_alloc.h>
+#include <vulkan/vulkan.hpp>
+
+namespace Orc
+{
+    std::shared_ptr<GraphicsDevice> createVulkanGraphicsDevice(void* windowHandle, uint32 width, uint32 height);
+    std::shared_ptr<GraphicsCommandList> createVulkanCommandList(GraphicsDevice* device, VkCommandPool cmdPool, GraphicsCommandList::GraphicsCommandListType type);
+}
 #else
-    namespace Orc { inline std::shared_ptr<GraphicsDevice> createVulkanGraphicsDevice(void* windowHandle, uint32 width, uint32 height) { return std::shared_ptr<GraphicsDevice>(); } }
+namespace Orc { inline std::shared_ptr<GraphicsDevice> createVulkanGraphicsDevice(void* windowHandle, uint32 width, uint32 height) { return std::shared_ptr<GraphicsDevice>(); } }
 #endif
